1031c7f29b64fc04eed12b7a3260fc80a4ff859b
LuisSambrano_UX <luissambranoarg@gmail.com>
2026-02-06 18:39:01 -0300

    fix(build): exclude node modules (fs, net, tls) from webpack to resolve edge deployment error

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding', 'dtrace-provider', 'bunyan')
+    config.externals.push('pino-pretty', 'lokijs', 'encoding', 'dtrace-provider', 'bunyan', 'fs', 'net', 'tls')
     return config
   },
 };
